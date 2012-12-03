976b75016b89ced059cdc2678c3039bfc039a27a
Kim Pepper <kim@pepper.id.au>
2012-12-04 10:26:22 +1100

    Updated javascript for redmine 2.1

--- assets/javascripts/boards_watchers.js
+++ assets/javascripts/boards_watchers.js
@@ -1,59 +1,54 @@
 function VVK_getWatchersCheckBoxes(watchers_dom_id) {
   var bw_users=$(watchers_dom_id+'-users');
 
-  return bw_users == null ? null : Prototype.Selector.select("input[type=checkbox]", bw_users);
+  return bw_users == null ? null : $(bw_users + "input:checkbox");
 }
 
-
 function toggleWatchersCheckBoxes(ids_to_toggle_str,on_off_str,watchers_dom_id) {
-    var user_ids=ids_to_toggle_str.evalJSON();
-    var turn_on=on_off_str.evalJSON();
-    var user_check_boxes=VVK_getWatchersCheckBoxes(watchers_dom_id);
+  var user_ids=jQuery.parseJSON(ids_to_toggle_str);
+  var turn_on=jQuery.parseJSON(on_off_str);
+  var user_check_boxes=VVK_getWatchersCheckBoxes(watchers_dom_id);
 
-    if(user_check_boxes!=null) {
-      var i;
-      for(i=0;i<user_check_boxes.length;i++) {
-        if(user_ids[0] == -1 || user_ids.indexOf(parseInt(user_check_boxes[i].value)) != -1) {
-            if(turn_on==-1) {
-              if(user_check_boxes[i].checked==0) {
-                turn_on=1;
-              } else {
-                turn_on=0;
-              }
-            }
-            user_check_boxes[i].checked=turn_on;
+  if(user_check_boxes!=null) {
+    var i;
+    for(i=0;i<user_check_boxes.length;i++) {
+      if(user_ids[0] == -1 || user_ids.indexOf(parseInt(user_check_boxes[i].value)) != -1) {
+        if(turn_on==-1) {
+          turn_on = !user_check_boxes[i].checked;
         }
+        user_check_boxes[i].checked=turn_on;
       }
     }
+  }
 }
 
 function highlightWatchers(watchers_dom_id) {
   var user_check_boxes=VVK_getWatchersCheckBoxes(watchers_dom_id);
 
   if(user_check_boxes!=null) {
-    var value=$(watchers_dom_id+'-search').value.toUpperCase();
+    var value=$('#'+watchers_dom_id+'-search').val().toUpperCase();
     var i;
     var user_name;
     var label_elem;
 
     for(i=0;i<user_check_boxes.length;i++) {
-      label_elem=user_check_boxes[i].up();
-      label_elem.removeClassName('bw-floating');
-      label_elem.removeClassName('bw-floating-select');
+      label_elem=$(user_check_boxes[i]).parent();
+      label_elem.removeClass('bw-floating');
+      label_elem.removeClass('bw-floating-select');
 
-      user_name=label_elem.readAttribute('full_text').toUpperCase();
+      user_name=label_elem.attr('full_text').toUpperCase();
 
-      if(value.length > 1 && user_name.include(value)) {
-        label_elem.addClassName('bw-floating-select');
+      if(value.length > 1 && user_name.indexOf(value) >= 0) {
+        label_elem.addClass('bw-floating-select');
       } else {
-        label_elem.addClassName('bw-floating');
+        label_elem.addClass('bw-floating');
       }
     }
   }
 }
 
 function toggleSelectedWatchers(on_off_str,watchers_dom_id) {
-  var turn_on=on_off_str.evalJSON();
+  var turn_on=jQuery.parseJSON(on_off_str);
   var user_check_boxes=VVK_getWatchersCheckBoxes(watchers_dom_id);
 
   if(user_check_boxes!=null) {
